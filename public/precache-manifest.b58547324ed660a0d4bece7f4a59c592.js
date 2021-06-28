0fa3e1d65b52c4e571409b99ee15a1282891182d
MajemiteJames <majemiteokoro@gmail.com>
2021-06-28 22:06:52 +0100

    deploying to heroku

---
+++ public/precache-manifest.b58547324ed660a0d4bece7f4a59c592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa2588162223156209ab",
+    "url": "/static/css/main.74b650fc.chunk.css"
+  },
+  {
+    "revision": "aa2588162223156209ab",
+    "url": "/static/js/main.aa258816.chunk.js"
+  },
+  {
+    "revision": "5932f0f2bdf93e9e334a",
+    "url": "/static/js/1.5932f0f2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "960b6d301c81b3eefa8dbf6cca9a8b37",
+    "url": "/index.html"
+  }
+];
